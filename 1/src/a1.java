56f7dfe6e6d0548272622cb62c26bc5de3b1a373
USER <USER@AMP-20>
2019-04-25 19:23:27 +0900

    commit

---
+++ 1/src/a1.java
@@ -0,0 +1,36 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class a1 extends JFrame {
+	public a1() {
+		setTitle("a1");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar mb = new JMenuBar();
+		JMenu A = new JMenu("ÆÄÀÏ");
+		mb.add(A);
+		setJMenuBar(mb);
+		
+		A.add(new JMenuItem("A"));
+		
+		
+		
+		setSize(500, 500);
+		setVisible(true);
+		
+		
+	}
+
+	
+	
+		public static void main(String[] args) {
+		new a1();
+	}
+
+}
